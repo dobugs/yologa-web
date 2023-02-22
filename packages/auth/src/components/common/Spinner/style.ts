54c1976747ed94a9316a684dd443b8418ea4f2c3
rgh <rheegon@gmail.com>
2023-02-22 23:54:35 +0900

    fix: 스피너 애니메이션 수정

--- packages/auth/src/components/common/Spinner/style.ts
+++ packages/auth/src/components/common/Spinner/style.ts
@@ -1,21 +1,23 @@
-import { css, SerializedStyles } from '@emotion/react';
+import { css, keyframes, SerializedStyles } from '@emotion/react';
+import { theme } from 'styles';
+
+const rotate = keyframes`
+  from { 
+    transform: rotate(0); 
+  }
+  to { 
+    transform: rotate(360deg); 
+  }
+`;
 
 const wrap = (color: string): SerializedStyles => css`
   position: relative;
-  width: auto;
-  height: auto;
-
-  .path {
-    transform-origin: center;
-    animation: rotate 0.75s infinite linear;
-    fill: ${color || '#e31c79'};
-  }
-
-  @keyframes rotate {
-    100% {
-      transform: rotate(360deg);
-    }
-  }
+  width: 48px;
+  height: 48px;
+  border: 6px solid ${color || '#e31c79'};
+  border-radius: 50%;
+  border-top-color: ${theme.color.base};
+  animation: ${rotate} 0.8s linear infinite;
 `;
 
 export { wrap };
--- packages/auth/src/styles/theme.ts
+++ packages/auth/src/styles/theme.ts
@@ -5,6 +5,7 @@ const theme: Theme = {
     white: '#FFFFFF',
     black: '#000000',
     primary: '#5f0080',
+    base: '#f2f2f2',
   },
 };
 
