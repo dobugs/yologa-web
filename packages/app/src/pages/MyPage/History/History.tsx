e66e4282589652e3e978314db9ca3a8a1cee1f1e
rgh <rheegon@gmail.com>
2023-03-05 12:07:09 +0900

    feat: favicon dobugs로 변경

--- packages/app/src/pages/MyPage/History/History.tsx
+++ packages/app/src/pages/MyPage/History/History.tsx
@@ -2,7 +2,9 @@ import { Page } from 'components/common';
 import React from 'react';
 
 function History() {
-  return <Page header="러닝 히스토리" description="여태껏 달려온 기록을 확인해보세요." content={<>리스트다</>} />;
+  return (
+    <Page type="LIST" header="러닝 히스토리" description="여태껏 달려온 기록을 확인해보세요." content={<>리스트다</>} />
+  );
 }
 
 export default History;
